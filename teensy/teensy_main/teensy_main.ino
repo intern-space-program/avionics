568ff2a76285bd22dfb7f4ec4df6415957c288d9
Amulek1416 <zcarico03@gmail.com>
2019-09-22 15:22:24 -0500

    Reformatted code. Added code to see if sensors are connected durring startup

--- teensy/teensy_main/teensy_main.ino
+++ teensy/teensy_main/teensy_main.ino
@@ -10,8 +10,6 @@
 
 
 //______________________________Libraries______________________________________//
-
-
 #include <ArduinoJson.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -19,17 +17,25 @@
 #include <Adafruit_BMP280.h>
 #include <utility/imumaths.h>
 #include <Adafruit_GPS.h>
-
-
+//_____________________________________________________________________________//
 
 
 //___________________________Global Assignments_________________________________//
-
+// IMU variables
+imu::Quaternion quat;
+imu::Vector<3> vaccel;
+imu::Vector<3> laccel;
+imu::Vector<3> gvector;
+imu::Vector<3> angular_vel;
+imu::Vector<3> magneto;
 
 //General Assignments ....................
 int packetNo = 0;
-bool BNO_CONNECTED;   // Flags to tell if the BMP and BNO sensors are connected
-bool BMP_CONNECTED;   // Flags to tell if the BMP and BNO sensors are connected
+
+// Flags to tell if the BNO, BMP, and GPS sensors are connected
+bool BNO_CONNECTED;
+bool BMP_CONNECTED;
+bool GPS_CONNECTED;
 uint32_t timer = millis();
 #define piSerial Serial2  
 
@@ -75,50 +81,81 @@ void setup(void)
 
 //____________________________________Startup Sequence_____________________________________// 
 
-//Start the BNO 055..................................................
-  if(!bno.begin()){
-    piSerial.print("No BNO055 Detected!");
-    while(1);
+//Start and set the BNO 055..................................................
+  uint8_t i;
+  for(; i < 3; i++) {
+    piSerial.print("On try %d:", i);
+    if(!bno.begin()) {
+      piSerial.print("No BNO055 Detected!");
+      BNO_CONNECTED = false;
     }
-  else
-    BNO_CONNECTED = true;
-
-//Start the BNO 055..................................................
-   if (!bmp.begin()) {
-    piSerial.println("No BMP280 Detected!");
-    while (1);
+    else {
+      piSerial.print("BNO055 Detected!");
+      BNO_CONNECTED = true;
+      break;
     }
-  else
-    BMP_CONNECTED = true;
+  }
   
-   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  if(BMP_CONNECTED){
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
-   *alti_offset = bmp.readAltitude(1013.25);
-   bno.setExtCrystalUse(true);
+    *alti_offset = bmp.readAltitude(1013.25);
+  }
 
-
-//Start the Adafruit GPS.........................................................
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(9600);
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  // GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ); // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-
-  // Request updates on antenna status, comment out to keep quiet
-  // GPS.sendCommand(PGCMD_ANTENNA);
+//Start and set the BNO 055..................................................
+  for(i = 0; i < 3; i++){
+    piSerial.print("On try %d:", i);
+    if (!bmp.begin()) {
+      piSerial.println("No BMP280 Detected!");
+      BMP_CONNECTED = false;
+    }
+    else {
+      piSerial.println("BMP280 Detected!");
+      BMP_CONNECTED = true;
+      break;
+    }
+  }
   
-  delay(1000);
+  if(BNO_CONNECTED){
+    bno.setExtCrystalUse(true);
+  }
+//.........................................................................//
+
+//Start and set the Adafruit GPS.............................................
+  for(i = 0; i < 3; i++){
+    piSerial.print("On try %d:", i);
+    // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+    if(!GPS.begin(9600);){
+      piSerial.println("No GPS Detected!");
+      GPS_CONNECTED = false;
+    }
+    else{
+      piSerial.println("GPS Detected!");
+      GPS_CONNECTED = true;
+      break;
+    }
+  }
+  if(GPS_CONNECTED){
+    // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+    // GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    // uncomment this line to turn on only the "minimum recommended" data
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+    // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+    // the parser doesn't care about other sentences at this time
+    // Set the update rate
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ); // 1 Hz update rate
+    // For the parsing code to work nicely and have time to sort thru the data, and
+    // print it out we don't suggest using anything higher than 1 Hz
+
+    // Request updates on antenna status, comment out to keep quiet
+    // GPS.sendCommand(PGCMD_ANTENNA);
+    
+    delay(1000);
+  }
 //__________________________________Final Handshake Sequence______________________________// 
 
   for(int i = 0; i < 3; i++){
@@ -137,22 +174,13 @@ void setup(void)
 
 
 ////////////////////////////////////////////////////////////////////////////////////
-//.................................Void Setup.....................................//
+//.................................Void Loop.....................................//
 ////////////////////////////////////////////////////////////////////////////////////
 
 void loop(void) {
-
-//_______________________________Check for Sensor Connection______________//
-//  BNO_CONNECTED = bno.isFullyCalibrated();
-//  BMP_CONNECTED = bmp.isConnected();
-
-//  Serial.println(BNO_CONNECTED);
-//  Serial.println(BMP_CONNECTED);
-
-
-//_______________________________________Sample GPS________________________//
-  char c = GPS.read();
-
+//_________________________________________________________________________//
+//_______________________________Sample GPS________________________________//
+//_________________________________________________________________________//
   if (GPS.newNMEAreceived()) {
     // a tricky thing here is if we print the NMEA sentence, or data
     // we end up not listening and catching other sentences!
@@ -161,68 +189,76 @@ void loop(void) {
     if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
       return; // we can fail to parse a sentence in which case we should just wait for another
   }
+
   // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis()) timer = millis();
-  if (millis() - timer > 100) {
-    timer = millis(); // reset the timer
+  if (timer > millis()) 
+    timer = millis();
 
-
- //
-
-    imu::Quaternion quat = bno.getQuat();           
-    imu::Vector<3> vaccel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-    imu::Vector<3> laccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-    imu::Vector<3> gvector = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
-    imu::Vector<3> angular_vel = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-    imu::Vector<3> magneto = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  
- //_________________________________Create JSON Array_________________________________________// 
-    StaticJsonDocument<550> doc;                      //PACKET SIZE = 550
-
-    JsonArray hdr = doc.createNestedArray("hdr");           //create the Json Object
-    JsonArray tpa = doc.createNestedArray("tpa");
-    JsonArray imu = doc.createNestedArray("imu");
-    JsonArray gps = doc.createNestedArray("gps");
-    hdr.add(packetNo);                      //each packet is assigned a sequential number
-    hdr.add(millis());                          //creates a millisecond readout based on the Arduino's internal clock    
-    //creates a json nested object for temp, press, alti
-  
-    tpa.add(bmp.readTemperature());       
-    tpa.add(bmp.readPressure());
-    tpa.add((bmp.readAltitude(1013.25))-(*alti_offset+.1));
+  /////////////////////////////////////////////////////////////////////////////////// 
+  //  READ SENSORS AND SEND DATA AT 10Hz
+  ///////////////////////////////////////////////////////////////////////////////////
+  if (millis() - timer > 100) 
+  {
+    //_____________Reset Timer______________//
+      timer = millis(); // reset the timer
+      
+    //_______________Read BNO_______________//
+      quat = bno.getQuat();           
+      vaccel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+      laccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+      gvector = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+      angular_vel = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+      magneto = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
     
-    imu.add(vaccel.x());
-    imu.add(vaccel.y());
-    imu.add(vaccel.z());
-    imu.add(quat.w());
-    imu.add(quat.x());
-    imu.add(quat.y());
-    imu.add(quat.z());
-    imu.add(laccel.x());
-    imu.add(laccel.y());
-    imu.add(laccel.z());
-    imu.add(gvector.x());
-    imu.add(gvector.y());
-    imu.add(gvector.z());
-    imu.add(angular_vel.x());
-    imu.add(angular_vel.y());
-    imu.add(angular_vel.z());
-    imu.add(magneto.x());
-    imu.add(magneto.y());
-    imu.add(magneto.z());
+    //___________Create JSON Array__________// 
+      StaticJsonDocument<550> doc; //PACKET SIZE = 550
 
-    // Serial.print(GPS.latitudeDegrees);
-    // Serial.print(GPS.longitudeDegrees);
-    gps.add(GPS.latitudeDegrees);
-    gps.add(GPS.longitudeDegrees);
-    gps.add(GPS.altitude);
+      JsonArray hdr = doc.createNestedArray("hdr"); //create the Json Object
+      JsonArray tpa = doc.createNestedArray("tpa");
+      JsonArray imu = doc.createNestedArray("imu");
+      JsonArray gps = doc.createNestedArray("gps");
+      hdr.add(packetNo); //each packet is assigned a sequential number
+      hdr.add(millis()); //creates a millisecond readout based on the Arduino's internal clock    
+      //creates a json nested object for temp, press, alti
 
-    serializeJson(doc, piSerial);
-    piSerial.println("");
+    //_____________Read TPA Data_____________//
+      tpa.add(bmp.readTemperature());       
+      tpa.add(bmp.readPressure());
+      tpa.add((bmp.readAltitude(1013.25))-(*alti_offset+.1));
+      
+    //________________Read IMU_______________//
+      imu.add(vaccel.x());
+      imu.add(vaccel.y());
+      imu.add(vaccel.z());
+      imu.add(quat.w());
+      imu.add(quat.x());
+      imu.add(quat.y());
+      imu.add(quat.z());
+      imu.add(laccel.x());
+      imu.add(laccel.y());
+      imu.add(laccel.z());
+      imu.add(gvector.x());
+      imu.add(gvector.y());
+      imu.add(gvector.z());
+      imu.add(angular_vel.x());
+      imu.add(angular_vel.y());
+      imu.add(angular_vel.z());
+      imu.add(magneto.x());
+      imu.add(magneto.y());
+      imu.add(magneto.z());
 
-    
-    
-    
-    packetNo++;
+    //_______________Read GPS______________//
+      // Serial.print(GPS.latitudeDegrees);
+      // Serial.print(GPS.longitudeDegrees);
+      gps.add(GPS.latitudeDegrees);
+      gps.add(GPS.longitudeDegrees);
+      gps.add(GPS.altitude);
+
+    //______________Send Data______________//
+      serializeJson(doc, piSerial);
+      piSerial.println("");
+      
+      packetNo++;
   }
+  ///////////////////////////////////////////////////////////////////////////////////
 }
