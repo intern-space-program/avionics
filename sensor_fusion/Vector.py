b63ca60a01196c42de3e050ed81b8d471aa9e01f
Mike Bernard <michael.bernard@uconn.edu>
2019-09-28 14:56:25 -0500

    update Vector.py

--- sensor_fusion/Vector.py
+++ sensor_fusion/Vector.py
@@ -8,7 +8,17 @@ A vector class that handles basic operations of
 vectors in R^3.
 '''
 
+from numpy import array, dot, cross
+
 
 class Vector:
-    pass
+    def __init__(self, x, y, z):
+        self.v = array([x, y, z])
 
+    @staticmethod
+    def vdot(v1, v2):
+        return dot(v1.v, v2.v)
+
+    @staticmethod
+    def vcross(v1, v2):
+        return cross(v1.v, v2.v)
