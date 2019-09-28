6610920e4e89bdec798bbb61d62d3b471c986c44
Mike Bernard <michael.bernard@uconn.edu>
2019-09-28 13:29:17 -0500

    create quaternion.py

---
+++ sensor_fusion/quaternion.py
@@ -0,0 +1,14 @@
+'''
+File name: quaternion.py
+Created by: Mike Bernard
+Creator email: mike.bernard@uconn.edu
+Creation date: 2019-09-28
+
+A quaternion class that handles basic operations of
+unit transform right quaternions.
+'''
+
+
+class Quaternion:
+    pass
+
