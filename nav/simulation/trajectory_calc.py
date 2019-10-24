70adc8e0e0416eb41d9216209aeeab48b5288f38
Ronnie Ankner <ronnieankner@gmail.com>
2019-10-23 21:26:32 -0500

    improved accuracy of trajectory script for simple parabolic motion

--- nav/simulation/trajectory_calc.py
+++ nav/simulation/trajectory_calc.py
@@ -1,110 +1,212 @@
-#script simulates "realistic" trajectories - DOES NOT MODEL THE AERODYNAMICS OF THE ROCKET
-#	inputs:
-#		- thrust curve
-#	outputs:
-#		- rocket state at each time step: 3DOF position, 3DOF velocity, 3DOF angle, 3DOF angular velocity
-#		- ideal sensor values based on rocket state
-#		- sensor output with noise, resolution, and clipping added
+#model assumes thruster in a specific location and the rocket always points toward the resultant of the velocity
+#thrust and drag always act in opposite direction and follow the resultant of velocity
+
 from math import cos
 from math import sin
+from math import sqrt
+import math
 import time
 
-class state_variable:
-	
-	def __init__(self, X, Y, Z):
-		self.X = X
-		self.Y = Y
-		self.Z = Z
+#============== MACROS ===============================
+
+#state variable 'macros'
+ZEROTH_ORDER = 0
+FIRST_ORDER = 1
+SECOND_ORDER = 2
+
+X = 0
+Y = 1
+Z = 2
+
+#Constant Macros
+PI = 3.1415926535
+
+
+#============== CLASSES ===============================
 
 class model_params:
 
-	def __init__(self):
-		self.max_phi = 0.524 #radians (about 30 degrees)
-		self.max_phi_accel = 0.1 # rad/s/time_step
-		self.max_theta_accel = 0.1 #rad/s/time_step
-		self.time_step = 0.01 #seconds
-		self.gravity = 9.8 #m/s^2
+    def __init__(self):
+        self.max_phi = 0.524 #radians (about 30 degrees)
+        self.max_phi_accel = 0.1 # rad/s/time_step
+        self.max_theta_accel = 0.1 #rad/s/time_step
+        self.time_step = 0.01 #seconds
+        self.gravity = 9.8 #m/s^2 
 
-class rocket: 
-	
-	def __init__(self):
-		self.position = state_variable(0.0, 0.0, 0.0) #m
-		self.velocity_vec = state_variable(0.0, 0.0, 0.0) #m/s
-		self.accel_vec = state_variable(0.0, 0.0, 0.0) #m/s^2
-		self.orientation = state_variable(0.0, 0.0, 0.0) #radians
-		self.angular_velocity =  state_variable(0.0, 0.0, 0.0)
-		self.phi = 0.0 # angle from Z axis
-		self.phi_rate = 0.0
-		self.theta = 0.0 # angle from X axis
-		self.theta_rate = 0.0
-		self.accel = 0.0 #m/s^2 magnitude pointing in direction of the rocket
-		self.velocity = 0.0 #m/s magnitude pointing in direction of the rocket
-		self.drag_const = 0 #0.5*Cd*A*p combined into one
-		self.thrust = 0.0 #Newtons
-		self.mass = 1 #Kg
-		self.model = model_params()
-	
-	def set_state(self):
-		self.accel_vec.Z = self.accel*cos(self.phi)
-		self.accel_vec.X = self.accel*sin(self.phi)*cos(self.theta)
-		self.accel_vec.Y = self.accel*sin(self.phi)*sin(self.theta)
+class rocket:
 
-		self.velocity_vec.Z = self.velocity*cos(self.phi)
-		self.velocity_vec.X = self.velocity*sin(self.phi)*cos(self.theta)
-		self.velocity_vec.Y = self.velocity*sin(self.phi)*sin(self.theta)
+    def __init__(self, start_v, start_a, start_phi, start_theta):
 
-	def update_state(self):
-		self.position.X += self.velocity_vec.X*self.model.time_step+0.5*self.accel_vec.X*self.model.time_step*self.model.time_step
-		self.position.Y += self.velocity_vec.Y*self.model.time_step+0.5*self.accel_vec.Y*self.model.time_step*self.model.time_step
-		self.position.Z += self.velocity_vec.Z*self.model.time_step+0.5*self.accel_vec.Z*self.model.time_step*self.model.time_step		
+        #Rocket Frame Variables
+        self.velocity = start_v #(m/s) magnitude which always points in direction of rocket
+        self.accel = start_a #(m/s^2) magnitude which always points in direction of rocket
+        #rocket_pos = [0, start_v, start_a] # (m, m/s, m/s^2) 3 DOF magnitude list | 0th is flight path distance, 1st is vel magnitude, 2nd is accel magnitude
+        self.phi = [start_phi, 0, 0] #(radians) 3 order list | 0th order phi is clockwise angle from Z axis
+        self.theta = [start_theta, 0, 0] #(radians) 3 order list | 0th order theta is clockwise angle from X axis
+        self.thrust = 0.0 # (Newtons) magnitude force always in direction of the rocket
 
-		self.accel = -(self.drag_const*self.velocity*self.velocity/self.mass)+(self.thrust/self.mass)
-		self.velocity += self.accel*self.model.time_step
+        #Rocket model properties
+        self.mass = 1 #(kg)
+        self.drag_const  = 0.01 #0.5*Cd*A*p all lumped together | reasonable is around 0.01 or less
 
-		self.accel_vec.Z = self.accel*cos(self.phi) - self.model.gravity
-		self.accel_vec.X = self.accel*sin(self.phi)*cos(self.theta)
-		self.accel_vec.Y = self.accel*sin(self.phi)*sin(self.theta)
+        #Overall Inertial Frame Variables
+        self.position = [[0,0,0],[0,0,0],[0,0,0]] # (m, m/s, m/s^2) 9DOF freedom position: 3DOF position, 3DOF velocity, 3DOF accel
+        self.position_name = ["Position", "Velocity", "Acceleration"]
+        self.angle = [[0,0,0],[0,0,0],[0,0,0]] # (rad, rad/s, rad/s^2) 9DOF freedom angle: 3DOF angle, 3DOF angular velocity, 3DOF angular acceleration
+        self.angle_name = ["Angle", "Angular Velocity", "Angular Acceleration"]
+        self.overall_time = 0.0
 
-		self.velocity_vec.X += self.accel_vec.X*self.model.time_step
-		self.velocity_vec.Y += self.accel_vec.Y*self.model.time_step
-		self.velocity_vec.Z += self.accel_vec.Z*self.model.time_step
-		
-		self.orientation.Z = cos(self.phi)
-		self.orientation.X = sin(self.phi)*cos(self.theta)
-		self.orientation.Y = sin(self.phi)*sin(self.theta)
+        #Flight Metrics
+        self.max_height = 0
+        self.max_speed = 0
+        self.max_velocity = 0
+        self.max_acceleration_mag = 0
+        self.max_acceleration_DOF = 0
+        self.flight_time = 0
+        self.distance_from_pad = 0
 
-	def print_position(self):
-		print("Position:\n\tX: %f | Y: %f | Z: %f"%(self.position.X, self.position.Y, self.position.Z))
-	def print_velocity(self):
-		print("Velocity:\n\tX: %f | Y: %f | Z: %f"%(self.velocity_vec.X, self.velocity_vec.Y, self.velocity_vec.Z))
-	def print_acceleration(self):
-		print("Acceleration:\n\tX: %f | Y: %f | Z: %f"%(self.accel_vec.X, self.accel_vec.Y, self.accel_vec.Z))
+        self.model = model_params()
+        self.print_each_step = False
 
-#initialization
-test_rocket = rocket()
-overall_time = 0
+    def __bool__(self):
+        if self.position[ZEROTH_ORDER][Z] > -0.001:
+            return True
+        else:
+            return False
 
-#starting conditions:
-test_rocket.velocity = 9.8
-test_rocket.phi = 0.523
-test_rocket.theta = 0.0
-test_rocket.set_state()
+    def transpose_ground_frame_to_rocket_frame(self):
+        self.velocity = sqrt(math.pow(self.position[FIRST_ORDER][X],2)+math.pow(self.position[FIRST_ORDER][Y],2)+math.pow(self.position[FIRST_ORDER][Z],2))
+        self.accel = sqrt(math.pow(self.position[SECOND_ORDER][X],2)+math.pow(self.position[SECOND_ORDER][Y],2)+math.pow(self.position[SECOND_ORDER][Z],2))
 
-while overall_time < 2:
-	print("Time: %f"%(overall_time))
-	test_rocket.print_position()
-	test_rocket.print_velocity()
-	test_rocket.print_acceleration()
-	test_rocket.update_state()
-	overall_time += test_rocket.model.time_step
-	print("--------------------------------------------------------------------")
-	time.sleep(5*test_rocket.model.time_step)
-	
+        for order in range(0,3):
+            self.phi[order] = math.acos(angle[order][Z])
+            if (angle[order][Y] >= 0):
+                self.theta[order] = math.acos(angle[order][X]/sin(self.phi[order]))
+            if (angle[order][Y] < 0):
+                self.theta[order] = 2*PI - math.acos(angle[order][X]/sin(self.phi[order]))
 
+    def transpose_rocket_frame_to_ground_frame(self):
 
+        for order in range(0,3):
+            self.angle[order][Z] = cos(self.phi[order])
+            self.angle[order][X] = sin(self.phi[order])*cos(self.theta[order])
+            self.angle[order][Y] = sin(self.phi[order])*sin(self.theta[order])
 
-		
-			
-			
-			
-		
+        for DOF in range(0,3):
+            self.position[FIRST_ORDER][DOF] = self.velocity*self.angle[ZEROTH_ORDER][DOF]
+            self.position[SECOND_ORDER][DOF] = self.accel*self.angle[ZEROTH_ORDER][DOF]
+
+        """self.position[FIRST_ORDER][Z] = self.velocity*cos(self.phi[ZEROTH_ORDER])
+        self.position[FIRST_ORDER][X] = self.velocity*sin(self.phi[ZEROTH_ORDER])cos(self.theta[ZEROTH_ORDER])
+        self.position[FIRST_ORDER][Z] = self.velocity*sin(self.phi[ZEROTH_ORDER])sin(self.theta[ZEROTH_ORDER]) 
+        self.position[SECOND_ORDER][Z] = self.accel*cos(self.phi[ZEROTH_ORDER])
+        self.position[SECOND_ORDER][X] = self.accel*sin(self.phi[ZEROTH_ORDER])cos(self.theta[ZEROTH_ORDER])
+        self.position[SECOND_ORDER][Z] = self.accel*sin(self.phi[ZEROTH_ORDER])sin(self.theta[ZEROTH_ORDER])"""
+
+    def print_position_vec(self, order):
+        print("%s:\n\tX: %f | Y: %f | Z: %f"%(self.position_name[order], self.position[order][X], self.position[order][Y], self.position[order][Z]))
+
+    def print_angle_vec(self, order):
+        print("%s:\n\tX: %f | Y: %f | Z: %f"%(self.angle_name[order], self.angle[order][X], self.angle[order][Y], self.angle[order][Z]))
+
+    def print_rocket_frame(self):
+        print("Positional:\n\tVelocity:     %f\n\tAcceleration: %f"%(self.velocity, self.accel))
+        print("Angular:\n\tPhi:   %f | %f\n\tTheta: %f | %f"%(self.phi[ZEROTH_ORDER], (self.phi[ZEROTH_ORDER]*180/PI), self.theta[ZEROTH_ORDER], (self.theta[ZEROTH_ORDER]*180/PI)))
+
+    def print_flight_metrics(self):
+        print("Max Height:              %f m"%(self.max_height))
+        print("Max Speed:               %f m/s"%(self.max_speed))
+        print("Max Velocity:            %f m/s"%(self.max_velocity))
+        print("Max Acceleration(mag):   %f m/s^2"%(self.max_acceleration_mag))
+        print("Max Acceleration(DOF):   %f m/s^2"%(self.max_acceleration_DOF))
+        print("\nFlight Time:             %f s"%(self.flight_time))
+        print("Distance from Pad:       %f m"%(self.distance_from_pad))
+       
+    def time_step_print(self):
+        print("Time: %f"%(self.overall_time))
+        for order in range(0,3):
+            self.print_position_vec(order)
+        self.print_rocket_frame()
+        print("----------------------------------------------------------------------------")
+        time.sleep(5*self.model.time_step)
+
+    def update_metrics(self):
+        self.flight_time = self.overall_time
+
+        if self.velocity > self.max_speed:
+            self.max_speed = self.velocity
+
+        if self.position[ZEROTH_ORDER][Z] > self.max_height:
+            self.max_height = self.position[ZEROTH_ORDER][Z]
+
+        for DOF in range(0,3):
+            if abs(self.position[FIRST_ORDER][DOF]) > self.max_velocity:
+                self.max_velocity = abs(self.position[FIRST_ORDER][DOF])
+            if abs(self.position[SECOND_ORDER][DOF]) > self.max_acceleration_DOF:
+                self.max_acceleration_DOF = abs(self.position[SECOND_ORDER][DOF])
+
+        accel_result = sqrt(math.pow(self.position[SECOND_ORDER][X],2)+math.pow(self.position[SECOND_ORDER][Y],2)+math.pow(self.position[SECOND_ORDER][Z],2))
+        if accel_result > self.max_acceleration_mag:
+            self.max_acceleration_mag = accel_result
+
+         self.distance_from_pad = sqrt(math.pow(self.position[ZEROTH_ORDER][X], 2) + math.pow(self.position[ZEROTH_ORDER][Y], 2))
+
+    def update_state(self):
+
+        self.update_metrics()
+
+        #update 1 Dimension acceleration for forces in direction of the rocket
+        self.accel = (-self.drag_const*self.velocity*self.velocity + self.thrust)/self.mass
+
+        #add random angle noise here
+        #propgate that through accel and vel to position
+       
+        #update 3DOF angle based on phi and theta at time t
+        for order in range(0,1):
+            self.angle[order][Z] = cos(self.phi[order])
+            self.angle[order][X] = sin(self.phi[order])*cos(self.theta[order])
+            self.angle[order][Y] = sin(self.phi[order])*sin(self.theta[order])
+
+        #break acceleration magnitude into 3 DOF accel and add 3DOF forces/accelerations
+        for DOF in range(0,3):
+            self.position[SECOND_ORDER][DOF] = self.accel*self.angle[ZEROTH_ORDER][DOF]
+        self.position[SECOND_ORDER][Z] -= self.model.gravity
+
+        #Acceleration(t) and velocity(t) update position(t+1)
+        #Acceleration(t) updates velocity(t+1)
+        for DOF in range(0,3):
+            self.position[ZEROTH_ORDER][DOF] += self.position[FIRST_ORDER][DOF]*self.model.time_step + 0.5*self.position[SECOND_ORDER][DOF]*self.model.time_step*self.model.time_step
+            self.position[FIRST_ORDER][DOF] += self.position[SECOND_ORDER][DOF]*self.model.time_step
+
+        #Get Velocity(t+1) magnitude
+        self.velocity = sqrt(math.pow(self.position[FIRST_ORDER][X],2)+math.pow(self.position[FIRST_ORDER][Y],2)+math.pow(self.position[FIRST_ORDER][Z],2))
+
+        #Update phi(t+1) and theta(t+1) based on velocity(t+1) resultant
+        temp_theta = math.atan((self.position[FIRST_ORDER][Y]/(self.position[FIRST_ORDER][X]+0.0000001)))
+        if (self.position[FIRST_ORDER][X] < 0):
+            #Quadrant II and III
+            temp_theta = PI + temp_theta
+        if (self.position[FIRST_ORDER][X] >= 0):
+            #Quadrant I and IV
+            temp_theta = 2*PI + temp_theta
+        self.theta[ZEROTH_ORDER] = temp_theta
+
+        temp_phi = math.acos((self.position[FIRST_ORDER][Z]/(self.velocity+0.000001)))
+        self.phi[ZEROTH_ORDER] = temp_phi
+
+        self.overall_time += self.model.time_step
+
+        if self.print_each_step:
+            self.time_step_print()
+
+test_rocket = rocket(0, 0, 0.0, 0.0)
+test_rocket.transpose_rocket_frame_to_ground_frame()
+
+while (test_rocket):
+    if (test_rocket.overall_time < 1):
+        test_rocket.thrust = 20.0
+    else:
+        test_rocket.thrust = 0.0
+    test_rocket.update_state()
+print("Simulation Ended")
+test_rocket.print_flight_metrics()
