0c1cefae13dd30198d726130a953eacbcaceeab4
Mike Bernard <michael.bernard@uconn.edu>
2019-10-29 17:20:19 -0400

    add merged_velocity_terms and weighted_avg functions

--- nav/NavMerge.py
+++ nav/NavMerge.py
@@ -11,7 +11,7 @@ from nav.constants import *
 
 class NavMerge:
     def __init__(self, prev_state, dt=0, airspeed=None, altitude=None, gps=None,
-                 delta_theta=None, accel_nc=None, accel=None):
+                 delta_theta=None, accel_nc=None, accel=None, sigmas=None):
         '''
         :param prev_state: Last known state vector
         :param dt: Delta time between last known state and current measurements
@@ -21,6 +21,7 @@ class NavMerge:
         :param delta_theta: Delta-angle vector (da_x, da_y, da_z) measured by IMU
         :param accel_nc: Non-conservative (no gravity) acceleration vector (d2x, d2y, d2z) measured by IMU
         :param accel: Conservative acceleration vector (d2x, d2y, d2z) measured by IMU
+        :param sigmas: The standard deviations of the sensors
 
         :type prev_state: `dict` of `string`: `np.array([])` and `string`: `float`
         :type dt: `float`
@@ -30,6 +31,7 @@ class NavMerge:
         :type delta_theta: `np.array([])` [1x3]
         :type accel_nc: `np.array([])` [1x3]
         :type accel: `np.array([])` [1x3]
+        :type sigmas: `dict`
 
         :return: `np.array([])`
         '''
@@ -41,6 +43,7 @@ class NavMerge:
         self.delta_theta = delta_theta
         self.accel_nc = accel_nc
         self.accel = accel
+        self.sigmas = sigmas
 
         self.accel_merged = self.merged_accel()
 
@@ -60,20 +63,14 @@ class NavMerge:
 
     def integrate_imu_linear(self):
         '''
-        Integrate IMU linear acceleration into linear
-        velocity and position.
+        Integrate IMU linear acceleration into linear velocity.
 
-        :return: `tuple` of (`numpy.array`, `numpy.array` of
-                 integrated position and velocity vectors.
+        :return: `numpy.array` of new velocity
         '''
         v_prev = self.prev_state['velocity']
-        p_prev = self.prev_state['position']
-
         v_new = v_prev + self.accel_merged*self.dt
-        p_new = p_prev + v_prev*self.dt + 0.5*self.accel_merged*self.dt**2
 
-        return p_new, v_new
-
+        return v_new
 
     def merge_airspeed_and_zdot(self):
         pass
@@ -81,8 +78,27 @@ class NavMerge:
     def merge_position_terms(self):
         pass
 
+    @staticmethod
+    def weighted_avg(values, weights):
+        '''
+        Takes a list of values and a list of weights associated
+        with those values (index-to-index) and returns a weighted
+        averaged of those values as a float.
+        '''
+        denom = sum([1/w**2 for w in weights])
+        num = sum([1/w**2 * v for v, w in zip(values, weights)])
+
+        return num/denom
+
     def merge_velocity_terms(self):
-        pass
+        v_new_i = self.integrate_imu_linear()
+        std_imu = self.sigmas['IMU']
+        std_airspeed = self.sigmas['airspeed']
+        v_new_mag = norm(v_new_i)
+        v_new_mag_est = self.weighted_avg([v_new_mag, self.airspeed],
+                                          [std_imu, std_airspeed])
+
+        return v_new_mag_est * v_new_i / v_new_mag
 
     def merge_attitude_quaternions(self):
         pass
