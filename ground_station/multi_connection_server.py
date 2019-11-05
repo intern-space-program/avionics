64c8586bd512f18962d9733772c4d634629d271a
Ronnie Ankner <ronnieankner@gmail.com>
2019-11-05 01:03:58 -0500

    SHITTON of multi-connection debugging, but it finally works :)

--- ground_station/multi_connection_server.py
+++ ground_station/multi_connection_server.py
@@ -1,6 +1,7 @@
 import socket
 import selectors
 from io import BytesIO
+import types
 
 SERVER_IP = ''
 SERVER_VID_PORT = 5000
@@ -9,7 +10,11 @@ SERVER_TELEM_PORT = 5001
 video_ports = [SERVER_VID_PORT]
 telem_ports = [SERVER_TELEM_PORT]
 
-stream_status = [1,1] #video, telemetry
+#video_ports = []
+#telem_ports = []
+
+connections = 0
+		
 
 def name_source(socket_obj):
 	port = socket_obj.getsockname()[1]
@@ -25,120 +30,136 @@ def name_source(socket_obj):
 	if (not(named)):
 		return False
 
-def accept_connection(socket_obj):
-	conn, addr = socket_obj.accept()
-	conn.setblocking(False)
-	if (name_source(socket_obj) == 'VIDEO'):
-		video_ports.append(addr[1])
-	if (name_source(socket_obj) == 'TELEMETRY'):
-		video_ports.append(addr[1])
-	print("%s Stream connected to %s:%d"%(name_source(socket_obj), addr[0], addr[1]))
-	data = types.SimpleNamespace(addr=addr, inb=b'', outb = b'')
-	events = selectors.EVENT_READ | selectors.EVENT_WRITE
-	sel.register(conn,events, data=data)
+class stream:
+	def __init__(self, name, server_socket, client_socket, store_file):
+		self.name = name
+		self.server_socket = server_socket
+		self.client_socket = client_socket
+		self.packet_cnt = 0
+		self.total_bytes = 0
+		self.store_file = store_file
+		self.file_handle = open(store_file, 'wb')
+		self.buffer = BytesIO()
+		self.alive = True
 	
+	def __bool__(self):
+		return self.alive
+
+	def store_buffer(self):
+		print("%s: Storing Buffer"%(self.name))
+		self.file_handle.write(self.buffer.getvalue())
+		
+	def get_buffer_size(self):
+		return self.buffer.getbuffer().nbytes
+
+	def clear_buffer(self):
+		print("%s: Clearing Buffer"%(self.name))
+		self.buffer.truncate(0)
+		self.buffer.seek(0)
+	
+	def close(self):
+		self.store_buffer()
+		self.clear_buffer()
+		self.file_handle.close()
+		self.server_socket.close()
+
+	def recv_new_packet(self):
+		packet = self.client_socket.recv(4096)
+		if (not(packet)):
+			print("%s: Stream ended, closing connection and file"%(self.name))
+			self.alive = False
+			self.close()
+			return
+		print("%s: New Packet | Size: %d Bytes"%(self.name, len(packet)))
+		self.buffer.write(packet)
+		self.packet_cnt += 1
+		self.total_bytes += len(packet)
+		
+		
+		
+
 
 server_vid_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_vid_sock.bind((SERVER_IP, SERVER_VID_PORT))
 server_vid_sock.listen()
-server_vid_sock.setblocking(False)
-print("%s Server Created and Listening"%(name_source(server_vid_sock)))
+print("%s Server Created and Listening on (%s, %d)"%(name_source(server_vid_sock), server_vid_sock.getsockname()[0], server_vid_sock.getsockname()[1]))
+
 
 server_telem_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_telem_sock.bind((SERVER_IP, SERVER_TELEM_PORT))
 server_telem_sock.listen()
-server_telem_sock.setblocking(False)
-print("%s Server Created and Listening"%(name_source(server_telem_sock)))
+print("%s Server Created and Listening on (%s, %d)"%(name_source(server_telem_sock), server_telem_sock.getsockname()[0], server_telem_sock.getsockname()[1]))
 
 sel = selectors.DefaultSelector()
 sel.register(server_vid_sock, selectors.EVENT_READ, data=None)
 sel.register(server_telem_sock, selectors.EVENT_READ, data=None)
 
-vid_buffer = BytesIO()
-telem_buffer = BytesIO()
-
 video_file = 'video_stream_recording.h264'
 telem_file = 'telemetry_stream.txt'
 
-video_handle = open(video_file, 'wb')
-telem_handle = open(telem_file, 'wb')
 
-vid_packet_cnt = 0
-telem_packet_cnt = 0
+#Wait for Video connection (First one to Initiate)
+vid_client, addr = server_vid_sock.accept()
+print("VIDEO Stream connected from (%s, %d)"%(addr[0], addr[1]))
+vid_client.setblocking(False)
+video_ports.append(addr[1])
+data = types.SimpleNamespace(addr=addr, inb=b'', outb = b'')
+events = selectors.EVENT_READ | selectors.EVENT_WRITE
+sel.register(vid_client,events, data=data)
 
-vid_total_bytes = 0
-telem_total_bytes = 0
 
-while stream_status[0] or stream_status[1]:
+#Wait for Telemetry connection(Second one to Initiate)
+telem_client, addr = server_telem_sock.accept()
+print("TELEMETRY Stream connected from (%s, %d)"%(addr[0], addr[1]))
+telem_client.setblocking(False)
+telem_ports.append(addr[1])
+data = types.SimpleNamespace(addr=addr, inb=b'', outb = b'')
+events = selectors.EVENT_READ | selectors.EVENT_WRITE
+sel.register(telem_client, events, data=data)
+
+#Now that connection order doesn't matter, set blocking to false
+server_vid_sock.setblocking(False)
+server_telem_sock.setblocking(False)
+
+#Create stream objects
+video_stream = stream('VIDEO', server_vid_sock, vid_client, video_file)
+telem_stream = stream('TELEMETRY', server_telem_sock, telem_client, telem_file)
+
+while video_stream or telem_stream:
 	events = sel.select(timeout=None)#BLOCKING, can set timeout to not block
 	for key, mask in events:
 		if key.data is None:
-			#initial connection attempt
-			accept_connection(key.fileobj)
-		else:
-			#data contains information to be read
+			print("CONNECTION ATTEMPT")
+		if key.data is not(None) and mask == selectors.EVENT_READ | selectors.EVENT_WRITE:
 			socket_obj = key.fileobj
-			recv_data = socket_obj.recv(4096)
-			if (not(recv_data)):
-				#Empty Data
-				#Socket Connection was broken
-				print("%s Stream BROKEN; Closing Connection"%(name_source(socket_obj)))
-				if (name_source(socket_obj) == 'VIDEO'):
-					stream_status[0] = 0
-				if (name_source(socket_obj) == 'TELEMETRY'):
-					stream_status[1] = 0
-				socket_obj.close()
-			else:
-				if name_source(key.fileobj) == 'VIDEO':
-					vid_packet_cnt += 1
-					print("VIDEO: Recieved packet #%d: %d Bytes"%(vid_packet_cnt, len(recv_data)))
-					vid_total_bytes += len(recv_data)
-					#Video Data is ready
-					vid_buffer.write(recv_data)
-					if (vid_buffer.getbuffer().nbytes() > 10000):
-						#Buffer reached thresholded size
-						print("VIDEO: Buffer reached capacity; storing and clearing")
-						#process data
-						#-> send to local udp port for display
+			if (name_source(socket_obj) == 'VIDEO' and video_stream):
+				video_stream.recv_new_packet()
+				if video_stream.get_buffer_size() > 10000:
+					#Buffer Reached Threshold
+	
+					#Process Buffer data
+		
+					#Store Buffer to file
+					video_stream.store_buffer()
+					
+					#Clear Buffer
+					video_stream.clear_buffer()
 
-						#store to file
-						video_handle.write(vid_buffer.get_value())
+			if (name_source(socket_obj) == 'TELEMETRY' and telem_stream):
+				telem_stream.recv_new_packet()
+				if telem_stream.get_buffer_size() > 2000:
+					#Buffer Reached Threshold
+	
+					#Process Buffer data
+		
+					#Store Buffer to file
+					telem_stream.store_buffer()
+					
+					#Clear Buffer
+					telem_stream.clear_buffer()
 
-						#clear buffer
-						vid_buffer.truncate(0)
-						vid_buffer.seek(0)
-
-				if name_source(key.fileobj) == 'TELEMETRY':
-					#Telemetry data is ready
-					telem_packet_cnt += 1
-					print("TELEMETRY: Recieved packet #%d: %d Bytes"%(telem_packet_cnt, len(recv_data)))
-					telem_total_bytes += len(recv_data)
-					telem_buffer.write(recv_data)
-					if (telem_buffer.getbuffer().nbytes() > 10000):
-						#Buffer reached thresholded size
-						print("TELEMETRY: Buffer reached capacity; storing and clearing")
-						#process data
-						#-> parse telemetry function here
-
-						#store to file
-						telem_handle.write(telem_buffer.get_value())
-
-						#clear buffer
-						telem_buffer.truncate(0)
-						telem_buffer.seek(0)
-
-				if not(name_source(key.fileobj)):
-					#Stream unidentified
-					print("Conneciton Unidentified") 
 print("Stream Ended")
-print("Stream Statistics: \n\tVIDEO Bytes: %d\n\tTELEM Bytes: %d"%(vid_total_bytes, telem_total_bytes))
-
-
-video_handle.close()
-telem_handle.close()
-
-server_vid_sock.close()
-server_telem_sock.close()
+print("Stream Statistics: \n\tVIDEO Bytes: %d\n\tTELEM Bytes: %d"%(video_stream.total_bytes, telem_stream.total_bytes))
 
 
 
