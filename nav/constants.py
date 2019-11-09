0e24319ae30751bc42b34b6c9084008aba94b19e
Mike Bernard <michael.bernard@uconn.edu>
2019-11-09 01:07:29 -0600

    add PASS/FAIL conventions

--- nav/constants.py
+++ nav/constants.py
@@ -1,3 +1,9 @@
+# Physical Constants
 G = 6.67408e-11  # (m**3/kg/s**2) gravitational constant
 M_E = 5.972e24   # (kg) mass of Earth
 G_E = G*M_E      # (m**3/s**2) gravitational parameter of Earth
+ACC_GRAV = 9.81  # (m/s**2) gravitational acceleration near Earth's surface
+
+# Conventions
+PASS = 0
+FAIL = 1
