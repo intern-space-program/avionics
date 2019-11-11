c0fad780dff79af8ad302f8000afb698b8f0e301
Mike Bernard <michael.bernard@uconn.edu>
2019-11-10 21:14:10 -0600

    add axis_angle_to_quaternion function

--- nav/utils/quaternion_utils.py
+++ nav/utils/quaternion_utils.py
@@ -10,7 +10,7 @@ Tools for dealing with scalar-first, transform, unit,
 right quaternions with Malcolm Shuster's conventions.
 '''
 
-from numpy import array, cross, dot, concatenate
+from numpy import array, cross, dot, concatenate, sin, cos
 from numpy.linalg import norm
 
 
@@ -54,3 +54,15 @@ def qconjugate(q):
     the inverse for unit quaternions.
     '''
     return concatenate([array([q[0]]), -1*q[1:]])
+
+
+def axis_angle_to_quaternion(axis, angle_rad):
+    '''
+    Convert an Euler axis and angle to a quaternion.
+    :param axis: `np.array([1x3])` (--) The axis of rotation
+    :param angle_rad: `float` (rad) The angle of rotation
+    :return: `np.array([1x4])` (--) Quaternion representing transformation
+    '''
+    s = array([cos(angle_rad/2.0)])
+    v = sin(angle_rad/2.0) * axis
+    return concatenate([s, v])
