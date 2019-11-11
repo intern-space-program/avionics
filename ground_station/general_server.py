e1f9a6c7bf0802971f38359a0a0b68537231a0fe
Ronnie Ankner <ronnieankner@gmail.com>
2019-11-11 01:50:11 -0600

    completely over hauled server

--- ground_station/general_server.py
+++ ground_station/general_server.py
@@ -3,11 +3,9 @@ import selectors
 from io import BytesIO
 import types
 import struct
-from pathlib import Path
 import os
 
-home = str(Path.home())
-print("Home Directory: %s"%(home))
+home = '/home/pi'
 store_dir = home + "/rocket_data"
 cmd = "mkdir " + store_dir
 os.system(cmd)
@@ -16,205 +14,336 @@ SERVER_IP = ''
 SERVER_VID_PORT = 5000
 SERVER_TELEM_PORT = 5001
 
-video_ports = [SERVER_VID_PORT]
-telem_ports = [SERVER_TELEM_PORT]
+SRC2SINK = 1
+SINK2SRC = 2
+DUPLEX = SRC2SINK|SINK2SRC
 
-def name_source(socket_obj):
-	port = socket_obj.getsockname()[1]
-	named = False
-	for ports in video_ports:
-		if port == ports:
-			named = True
-			return 'VIDEO'
-	for ports in telem_ports:
-		if port == ports:
-			named = True
-			return 'TELEMETRY'
-	if (not(named)):
-		return False
-
-class data_point:
-	def __init__(self, packet_num, time_stamp, status_list, pos_list, vel_list, orientation_list):
-		self.packet_num = packet_num
-		self.time_stamp = time_stamp
-		self.status = status_list #[imu, gps, alt, teensy, raspi, LTE, serial]
-		self.position = pos_list #[X, Y, Z]
-		self.velocity = vel_list #[X, Y, Z]
-		self.orientation = orientation_list #[Roll, Pitch, Yaw]
-	
-	def print_data_point(self):
-		print("Data Point:")
-		print("\tPacket Number: %d"%(self.packet_num))
-		print("\tTime Stamp: %d"%(self.time_stamp))
-		print("\t        IMU  GPS  ALT  Teensy  Raspi  LTE  Serial")
-		print("\tStatus:  %d   %d    %d      %d        %d       %d        %d"%(self.status[0], self.status[1], self.status[2], self.status[3], self.status[4], self.status[5], self.status[6]))
-		print("\t             X       Y       Z")
-		print("\tPosition:  %.2f    %.2f    %.2f"%(self.position[0], self.position[1], self.position[2]))
-		print("\t             X       Y       Z")
-		print("\tVelocity:  %.2f    %.2f    %.2f"%(self.velocity[0], self.velocity[1], self.velocity[2]))
-		print("\t             X       Y       Z")
-		print("\tOrientat:  %.2f    %.2f    %.2f"%(self.orientation[0], self.orientation[1], self.orientation[2]))
-
-class stream:
-	def __init__(self, name, server_socket, client_socket, store_file):
+class server_stream:
+	def __init__(self, name, server_IP, server_port, selector_obj, src2sink_file, sink2src_file, mode, buffer_thresh):
 		self.name = name
-		self.server_socket = server_socket
-		self.source_sockets = []
+		self.server_IP = server_IP
+		self.server_port = server_port
+		self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.undeclared_sockets = []
 		self.sink_sockets = []
-		self.packet_cnt = 0
-		self.total_bytes = 0
-		self.store_file = store_file
-		self.file_handle = open(store_file, 'wb')
-		self.buffer = BytesIO()
+		self.src_sockets = []
+		self.src2sink_file = src2sink_file
+		self.sink2src_file = sink2src_file
+		self.mode = mode #can be SRC2SINK, SINK2SRC, or DUPLEX (SRC2SINK|SINK2SRC)
+		self.buffer_thresh = buffer_thresh
+		self.downstream_file = False
+		self.upstream_file = False
+		self.print_output = True #Controls Formatted output to the screen
+		self.log_output = True #controls formatted output to overall system log file
+		
+		if (mode & SRC2SINK == SRC2SINK):
+			self.src2sink_buffer = BytesIO()
+			if self.src2sink_file is not None:
+				self.src2sink_file_handle = open(self.src2sink_file, 'wb')
+				self.downstream_file = True
+
+		if (mode & SINK2SRC == SINK2SRC):
+			self.sink2src_buffer = BytesIO()
+			if self.sink2src_file is not None:
+				self.sink2src_file_handle = open(self.sink2src_file, 'wb')
+				self.upstream_file = True
+		
+		self.server_socket.bind((self.server_IP, self.server_port))
+		self.server_socket.listen()
+		self.server_socket.setblocking(False)
+		selector_obj.register(self.server_socket, selectors.EVENT_READ, data=None)
+		print("%s: Server and stream created"%(self.name))
 		self.alive = True
+		
+		#Statistics [OPTIONAL]
+		self.recv_packet_cnt = 0
+		self.recv_total_bytes = 0
+		self.send_packet_cnt = 0
+		self.send_total_bytes = 0
+		
 	
 	def __bool__(self):
 		return self.alive
-
-	def store_buffer(self):
-		print("%s: Storing Buffer"%(self.name))
-		self.file_handle.write(self.buffer.getvalue())
-		
-	def get_buffer_size(self):
-		return self.buffer.getbuffer().nbytes
-
-	def clear_buffer(self):
-		print("%s: Clearing Buffer"%(self.name))
-		self.buffer.truncate(0)
-		self.buffer.seek(0)
 	
-	def close(self):
-		self.store_buffer()
-		self.clear_buffer()
-		self.file_handle.close()
-		self.server_socket.close()
+	def print_statistics(self):
+		mode_name = ['SRC2SINK', 'SINK2SRC', 'SRC2SINK|SINK2SRC']
+		#print("Stream Name: %s"%(self.name))
+		#print("Stream Mode: %s"%(mode_name[self.mode-1]))
+		#print("Read:\n\tPackets:     %d\n\tTotal Bytes: %d"%(self.recv_packet_cnt, self.recv_total_bytes))
+		#print("Write:\n\tPackets:     %d\n\tTotal Bytes: %d"%(self.send_packet_cnt, self.send_total_bytes))
 
-	def recv_new_packet(self):
-		packet = self.client_socket.recv(4096)
-		if (not(packet)):
-			print("%s: Stream ended, storing, then closing connection and file"%(self.name))
-			self.alive = False
-			self.close()
+	def stream_print(self, msg):
+		if (not(self.print_output)):
 			return
-		print("%s: New Packet | Size: %d Bytes"%(self.name, len(packet)))
-		self.buffer.write(packet)
-		self.packet_cnt += 1
-		self.total_bytes += len(packet)
+		print("%s: %s"%(self.name, msg))
+
+	def log_print(self, msg):
+		if (not(self.log_output)):
+			return
+		log_start("%s: %s"%(self.name, msg))
+
+	def claim_socket(self, socket_obj):
+		for socket in self.undeclared_sockets:
+			if socket_obj == socket:
+				return True
+		for socket in self.sink_sockets:
+			if socket_obj == socket:
+				return True
+		for socket in self.src_sockets:
+			if socket_obj == socket:
+				return True
+		return False
+	
+	def handle_connection(self, selector_obj):
+		client_sock, addr = self.server_socket.accept()
+		self.stream_print("UNDECLARED Stream connected from (%s, %d)"%(addr[0], addr[1]))
+		client_sock.setblocking(False)
+		self.undeclared_sockets.append(client_sock)
+		data = self.server_socket #make the client events identifiable by its server socket
+		events = selectors.EVENT_READ | selectors.EVENT_WRITE
+		selector_obj.register(client_sock, events, data=data)
+		client_sock.sendall("Please register as 'sink' or 'src'")
 		
 
-def parse_telemetry(telem_packets, data_point_buffer):
-	code_word = bytearray([192,222]) #0xC0DE (Beginning of Packet)
-	EOP = bytearray([237,12]) #0xED0C (End of Packet)
-	packet_list = telem_packets.split(code_word)
-	for packets in packet_list:
-		good_packet = False
-		try:
-			data = struct.unpack('>ii???????fffffffffh', packets)
-			if packets[51:53] == EOP:
-				good_packet = True
-			else:
-				print("End of Packet check failed")
-				print("\tPacket End: %s"%(packets[51:53]))
-				print("\tEOP:        %s"%(EOP))
-		except:
-			print("BAD PACKET")
+	def close(self):
+		self.stream_print("RUNNING FULL CLOSE")
+		if (not(self.mode & SRC2SINK)):
+			pass
+		else:
+			self.store_buffer(SRC2SINK)
+			self.clear_buffer(SRC2SINK)
+			self.close_file(SRC2SINK)
 
-		if (good_packet):
-			status_list = [data[2], data[3], data[4], data[5], data[6], data[7], data[8]]
-			pos_list = [data[9], data[10], data[11]]
-			vel_list = [data[12], data[13], data[14]]
-			orient_list = [data[15], data[16], data[17]]
-			new_data = data_point(data[0], data[1], status_list, pos_list, vel_list, orient_list)
-			new_data.print_data_point()
-			data_point_buffer.append(new_data)
+		if (not(self.mode & SINK2SRC)):
+			pass
+		else:
+			self.store_buffer(SINK2SRC)
+			self.clear_buffer(SINK2SRC)
+			self.close_file(SINK2SRC)
+
+		self.stream_print("Closing Server Socket")
+		self.server_socket.close()
+		self.alive = False
 	
-	return data_point_buffer
+	def close_socket(self, socket_obj, selector_obj):
+		for socket in self.undeclared_sockets:
+			if socket_obj == socket:
+				self.undeclared_sockets.remove(socket_obj)
+				selector_obj.unregister(socket_obj)
+				socket_obj.close()
+				return
+		for socket in self.sink_sockets:
+			if socket_obj == socket:
+				self.undeclared_sockets.remove(socket_obj)
+				selector_obj.unregister(socket_obj)
+				socket_obj.close()
+				return
+		for socket in self.src_sockets:
+			if socket_obj == socket:
+				self.undeclared_sockets.remove(socket_obj)
+				selector_obj.unregister(socket_obj)
+				socket_obj.close()
+				return
+
+	def close_file(self, mode):
+		if (mode & SRC2SINK == SRC2SINK):
+			if (not(self.mode & SRC2SINK)):
+				self.stream_print("DOWNSTREAM ACCESS DENIED")
+				return
+			if(not(self.donstream_file)):
+				return
+			self.stream_print("Closing DOWNSTREAM File")
+			self.src2sink_handle.close()
+			self.downstream_file = False
+
+		if (mode & SINK2SRC == SINK2SRC):
+			if (not(self.mode & SINK2SRC)):
+				self.stream_print("UPSTREAM ACCESS DENIED")
+				return
+			if(not(self.upstream_file)):
+				return
+			self.stream_print("Closing UPSTREAM File")
+			self.sink2src_handle.close()
+			self.upstream_file = False
+
+	def store_buffer(self, mode):
+		if (mode & SRC2SINK == SRC2SINK):
+			if (not(self.mode & SRC2SINK)):
+				self.stream_print("SRC2SINK ACCESS DENIED")
+				return
+			if(not(self.downstream_file)):
+				return
+			self.stream_print("Storing SRC2SINK Buffer: %d Bytes"%(self.get_buffer_size(SRC2SINK)))
+			self.src2sink_file_handle.write(self.src2sink_buffer.getvalue())
+
+		if (mode & SINK2SRC == SINK2SRC):
+			if (not(self.mode & SINK2SRC)):
+				self.stream_print("SINK2SRC ACCESS DENIED")
+				return
+			if(not(self.upstream_file)):
+				return
+			self.stream_print("Storing SINK2SRC Buffer: %d Bytes"%(self.get_buffer_size(SINK2SRC)))
+			self.sink2src_file_handle.write(self.sink2src_buffer.getvalue())
+		
+	def get_buffer_size(self, mode):
+		if (mode & SRC2SINK == SRC2SINK):
+			if (not(self.mode & SRC2SINK)):
+				self.stream_print("NO DOWNSTREAM BUFFER")
+				return
+			return self.src2sink_buffer.getbuffer().nbytes
+
+		if (mode & SINK2SRC == SINK2SRC):
+			if (not(self.mode & SINK2SRC)):
+				self.stream_print("NO UPSTREAM BUFFER")
+				return
+			return self.sink2src_buffer.getbuffer().nbytes
+
+	def clear_buffer(self, mode):
+		if (mode & SRC2SINK == SRC2SINK):
+			if (not(self.mode & SRC2SINK)):
+				self.stream_print("NO SRC2SINK BUFFER")
+				return
+			if (self.get_buffer_size(SRC2SINK) == 0):
+				return
+			self.stream_print("Clearing SRC2SINK Buffer")
+			self.src2sink_buffer.truncate(0)
+			self.src2sink_buffer.seek(0)
+
+		if (mode & SINK2SRC == SINK2SRC):
+			if (not(self.mode & SINK2SRC)):
+				self.stream_print("NO SINK2SRC BUFFER")
+				return
+			if (self.get_buffer_size(SINK2SRC) == 0):
+				return
+			self.stream_print("Clearing SINK2SRC Buffer")
+			self.sink2src_buffer.truncate(0)
+			self.sink2src_buffer.seek(0)
+	
+	def add_to_buffer(self, msg, mode):
+		if (mode & SRC2SINK == SRC2SINK):
+			if (not(self.mode & SRC2SINK)):
+				self.stream_print("NO SRC2SINK BUFFER")
+				return
+			self.stream_print("Adding to SRC2SINK Buffer")
+			self.src2sink_buffer.write(msg)
+
+		if (mode & SINK2SRC == SINK2SRC):
+			if (not(self.mode & SINK2SRC)):
+				self.stream_print("NO SINK2SRC BUFFER")
+				return
+			self.stream_print("Adding to SINK2SRC Buffer")
+			self.SINK2SRC_buffer.write(msg)
+	
+	def send_packet(self, msg, direction, selector_obj):
+		if (direction & SRC2SINK == SRC2SINK):
+			if (not(self.mode & SRC2SINK)):
+				self.stream_print("NO SRC2SINK ACCESS")
+				return
+			self.stream_print("Sending Packet from SRC(s) -> SINK(s)")
+			for socket in sink_sockets:
+				try: 
+					socket.sendall(msg)
+				except:
+					#Broken Pipe error; Socket no longer connected
+					self.close_socket(socket, selector_obj)
+
+		if (direction & SINK2SRC == SINK2SRC):
+			if (not(self.mode & SINK2SRC)):
+				self.stream_print("NO SINK2SRC ACCESS")
+				return
+			self.stream_print("Sending Packet from SINK(s) -> SRC(s)")
+			for socket in src_sockets:
+				try: 
+					socket.sendall(msg)
+				except:
+					#Broken Pipe error; Socket no longer connected
+					self.close_socket(socket, selector_obj)
+					
+
+		
+	def recv_new_packet(self, socket_obj, selector_obj):
+		if (not(self.alive)):
+			return
+		data_packet = socket_obj.recv(4096)
+		if (not(data_packet)):
+			#Data is empty; socket closed by them
+			self.close_socket(socket_obj, selector_obj)
+			return
+		if "KILL STREAM" in data_packet:
+			#kill switch for whole network -> ends Server and all clients
+			self.send_packet(data_packet, SRC2SINK, selector_obj)
+			self.send_packet(data_packet, SINK2SRC, selector_obj)
+			self.close()
+
+		#Data is not empty or kill switch. Check to see where data came from and where it should go
+		if socket_obj in self.undeclared_sockets:
+			if "sink" in data_packet:
+				self.undeclared_sockets.remove(socket_obj)
+				self.sink_sockets.append(socket_obj)
+			elif "src" in data_packet:
+				self.undeclared_sockets.remove(socket_obj)
+				self.src_sockets.append(socket_obj)
+			else:
+				self.stream_print("UNRECOGNIZED DATA FROM UNDECLARED SOCKET")
+			return
+			
+		if socket_obj in self.sink_sockets:
+			direction = SINK2SRC
+			self.add_to_buffer(data_packet, direction)
+			self.send_packet(data_packet, direction, selector_obj)
+			if (self.get_buffer_size(direction) > self.buffer_thresh):
+				self.store_buffer(direction)
+				self.clear_buffer(direction)
+			return
+
+		if socket_obj in self.src_sockets:
+			direction = SRC2SINK
+			self.add_to_buffer(data_packet, direction)
+			self.send_packet(data_packet, direction, selector_obj)
+			if (self.get_buffer_size(direction) > self.buffer_thresh):
+				self.store_buffer(direction)
+				self.clear_buffer(direction)
+			return
+		
+		
 
 
-server_vid_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_vid_sock.bind((SERVER_IP, SERVER_VID_PORT))
-server_vid_sock.listen()
-print("%s Server Created and Listening on (%s, %d)"%(name_source(server_vid_sock), server_vid_sock.getsockname()[0], server_vid_sock.getsockname()[1]))
+#==================================================== CODE ==============================================
 
-
-server_telem_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_telem_sock.bind((SERVER_IP, SERVER_TELEM_PORT))
-server_telem_sock.listen()
-print("%s Server Created and Listening on (%s, %d)"%(name_source(server_telem_sock), server_telem_sock.getsockname()[0], server_telem_sock.getsockname()[1]))
+#def __init__(self, name, server_IP, server_port, selector_obj, src2sink_file, sink2src_file, mode, buffer_thresh):
 
 sel = selectors.DefaultSelector()
-sel.register(server_vid_sock, selectors.EVENT_READ, data=None)
-sel.register(server_telem_sock, selectors.EVENT_READ, data=None)
 
 video_file = store_dir + '/video_stream_recording.h264'
 telem_file = store_dir + '/telemetry_stream.txt'
-
-
-#Wait for Video connection (First one to Initiate)
-vid_client, addr = server_vid_sock.accept()
-print("VIDEO Stream connected from (%s, %d)"%(addr[0], addr[1]))
-vid_client.setblocking(False)
-video_ports.append(addr[1])
-data = types.SimpleNamespace(addr=addr, inb=b'', outb = b'')
-events = selectors.EVENT_READ | selectors.EVENT_WRITE
-sel.register(vid_client,events, data=data)
-
-
-#Wait for Telemetry connection(Second one to Initiate)
-telem_client, addr = server_telem_sock.accept()
-print("TELEMETRY Stream connected from (%s, %d)"%(addr[0], addr[1]))
-telem_client.setblocking(False)
-telem_ports.append(addr[1])
-data = types.SimpleNamespace(addr=addr, inb=b'', outb = b'')
-events = selectors.EVENT_READ | selectors.EVENT_WRITE
-sel.register(telem_client, events, data=data)
-
-#Now that connection order doesn't matter, set blocking to false
-server_vid_sock.setblocking(False)
-server_telem_sock.setblocking(False)
+command_file = store_dir + '/command_stream.txt'
 
 #Create stream objects
-video_stream = stream('VIDEO', server_vid_sock, vid_client, video_file)
-telem_stream = stream('TELEMETRY', server_telem_sock, telem_client, telem_file)
+video_stream = server_stream('VIDEO', SERVER_IP, SERVER_VID_PORT, sel, video_file, None, SRC2SINK, 10000)
+telem_stream = server_stream('TELEMETRY',SERVER_IP, SERVER_TELEM_PORT, sel, telem_file, command_file, DUPLEX, 2000)
 
-data_point_buffer = []
 
 while video_stream or telem_stream:
 	events = sel.select(timeout=0.1)#BLOCKING, can set timeout to not block
 	for key, mask in events:
+		socket_obj = key.file_obj
 		if key.data is None:
 			print("CONNECTION ATTEMPT")
+			if socket_obj == video_stream.server_socket:
+				video_stream.handle_connection(sel)
+			if socket_obj == telem_stream.server_socket:
+				telem_stream.handle_connection(sel)
+
 		if key.data is not(None) and mask == selectors.EVENT_READ | selectors.EVENT_WRITE:
 			socket_obj = key.fileobj
-			if (name_source(socket_obj) == 'VIDEO' and video_stream):
-				video_stream.recv_new_packet()
-				if video_stream.get_buffer_size() > 10000:
-					#Buffer Reached Threshold
-	
-					#Process Buffer data
-		
-					#Store Buffer to file
-					video_stream.store_buffer()
-					
-					#Clear Buffer
-					video_stream.clear_buffer()
+			if (key.data == video_stream.sever_socket and video_stream):
+				video_stream.recv_new_packet(socket_obj, sel)
 
 			if (name_source(socket_obj) == 'TELEMETRY' and telem_stream):
-				telem_stream.recv_new_packet()
-				if telem_stream.get_buffer_size() > 500:
-					#Buffer Reached Threshold
-	
-					#Process Buffer data
-					data_point_buffer = parse_telemetry(telem_stream.buffer.getvalue(), data_point_buffer)
-
-					#Store Buffer to file
-					telem_stream.store_buffer()
-					
-					#Clear Buffer
-					telem_stream.clear_buffer()
+				telem_stream.recv_new_packet(socket_obj, sel)
+				
 
 print("Stream Ended")
-print("Stream Statistics: \n\tVIDEO Bytes: %d\n\tTELEM Bytes: %d"%(video_stream.total_bytes, telem_stream.total_bytes))
 
 
 
