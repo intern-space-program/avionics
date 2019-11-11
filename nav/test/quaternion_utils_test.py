51326b7beae54be65d94be571dc03be6828d24d2
Mike Bernard <michael.bernard@uconn.edu>
2019-11-10 19:45:47 -0600

    add qconjugate null and value tests

--- nav/test/quaternion_utils_test.py
+++ nav/test/quaternion_utils_test.py
@@ -85,13 +85,51 @@ def qnorm_test_values():
         return FAIL, description
 
 
+def qconjugate_test_null():
+    # setup
+    description = 'qconjugate_test_null - Test qconjugate with zeroed-out input'
+    q = array([0.0, 0.0, 0.0, 0.0])
+
+    # expected results
+    exp = array([0.0, 0.0, 0.0, 0.0])
+
+    # unit test
+    ret = qconjugate(q)
+
+    # results
+    if allclose(ret, exp, atol=0.001):
+        return PASS, description
+    else:
+        return FAIL, description
+
+
+def qconjugate_test_values():
+    # setup
+    description = 'qconjugate_test_null - Test qconjugate with non-zero input'
+    q = array([0.5, 0.5, 0.5, 0.5])
+
+    # expected results
+    exp = array([0.5, -0.5, -0.5, -0.5])
+
+    # unit test
+    ret = qconjugate(q)
+
+    # results
+    if allclose(ret, exp, atol=0.001):
+        return PASS, description
+    else:
+        return FAIL, description
+
+
 def main():
     module_name = 'quaternion_utils.py'
     tests = [
         qnorm_test_null,
         qnorm_test_values,
         qcomp_test_null,
-        qcomp_test_values
+        qcomp_test_values,
+        qconjugate_test_null,
+        qconjugate_test_values
     ]
 
     unit_test(module_name, tests)
