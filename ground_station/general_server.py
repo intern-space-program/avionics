063fffd9ad1016bd44658569d9e46adbe891a116
Ronnie Ankner <ronnieankner@gmail.com>
2019-11-15 12:08:38 -0500

    Did a push without testing :/

--- ground_station/general_server.py
+++ ground_station/general_server.py
@@ -380,16 +380,25 @@ class server_stream:
 				self.close()
 
 	def query_users(self, socket_obj, selector_obj):
-		msg = ""
-		msg += "UNDECLARED Sockets (%d): "%(len(self.undeclared_sockets))
+		msg = "UNDECLARED Sockets (%d): "%(len(self.undeclared_sockets))
 		for socket in self.undeclared_sockets:
 			msg += self.print_socket(socket)
+		
+		try:
+			socket_obj.sendall(msg.encode('utf-8'))
+		except:
+			self.close_socket(socket_obj, selector_obj)
 
-		msg += " | SINK Sockets (%d): "%(len(self.undeclared_sockets))
+		msg = "SINK Sockets (%d): "%(len(self.sink_sockets))
 		for socket in self.sink_sockets:
 			msg += self.print_socket(socket)
 
-		msg += " | SRC Sockets (%d): "%(len(self.undeclared_sockets))
+		try:
+			socket_obj.sendall(msg.encode('utf-8'))
+		except:
+			self.close_socket(socket_obj, selector_obj)
+
+		msg = "SRC Sockets (%d): "%(len(self.src_sockets))
 		for socket in self.src_sockets:
 			msg += self.print_socket(socket)
 	
