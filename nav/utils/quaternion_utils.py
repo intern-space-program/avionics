b1cae0309b75d52584a6c5d91bab5e89dc4c22a9
Mike Bernard <mike.bernard@uconn.edu>
2020-02-17 21:30:46 -0500

    improve numpy array efficiency a bit
    closes #32

--- nav/utils/quaternion_utils.py
+++ nav/utils/quaternion_utils.py
@@ -7,49 +7,36 @@ Tools for dealing with scalar-first, transform, unit,
 right quaternions with Malcolm Shuster's conventions.
 '''
 
-from numpy import array, cross, dot, concatenate, sin, cos
+from numpy import array, zeros, cross, dot, concatenate, sin, cos
 from numpy.linalg import norm
 
 
 def qcomp(q1, q2):
-    '''
-    Compose two quaternions.
-    '''
-    q1s = q1[0]
-    q1v = array(q1[1:])
-    q2s = q2[0]
-    q2v = array(q2[1:])
+    ''' Compose two quaternions.'''
+    q1s, q1v = q1[0], q1[1:]
+    q2s, q2v = q2[0], q2[1:]
 
-    s = array([q1s * q2s - dot(q2v, q1v)])
+    s = q1s*q2s - dot(q2v, q1v)
     v = q1s*q2v + q2s*q1v - cross(q1v, q2v)
 
-    return concatenate([s, v])
+    return concatenate([array([s]), v])
 
 
 def qnorm(q):
-    '''
-    Normalize a quaternion.
-    '''
-    if norm(q) != 0.0:
-        return q/norm(q)
-    else:
-        return array([0.0, 0.0, 0.0, 0.0])
+    ''' Normalize a quaternion. '''
+    return q/norm(q) if norm(q) != 0.0 else zeros(4)
 
 
 def qvectransform(q, v):
-    '''
-    Transform a vector's frame.
-    '''
+    ''' Transform a vector's frame. '''
     qvec = concatenate([array([0]), v])
     transformed = qcomp(q, qcomp(qvec, qconjugate(q)))
     return transformed[1:]
 
 
 def qconjugate(q):
-    '''
-    Get the conjugate of a quaternion. This is equal to
-    the inverse for unit quaternions.
-    '''
+    ''' Get the conjugate of a quaternion. This is equal to
+    the inverse for unit quaternions. '''
     return concatenate([array([q[0]]), -1*q[1:]])
 
 
