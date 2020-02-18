3c83304647a25fd42bd7293e2b63b835a6bc0764
Mike Bernard <mike.bernard@uconn.edu>
2020-02-17 19:05:14 -0500

    fix bug in merge_accel

--- nav/utils/frame_utils.py
+++ nav/utils/frame_utils.py
@@ -18,13 +18,11 @@ def lla_to_ecef(lla_array):
     Reference:
     https://www.mathworks.com/help/aeroblks/llatoecefposition.html
 
-    :param lla_array: `numpy.array([1x3])` array of (lat, long, alt) as `float`
-    :return: `numpy.array([1x3])` ECEF (x, y, z) position
+    :param lla_array: `numpy.array([1x3])` array of (lat, long, alt) [deg, deg, m] as `float`
+    :return: `numpy.array([1x3])` ECEF (x, y, z) [m] position
     '''
     # unpack lla_array
-    lat = lla_array[0]
-    long = lla_array[1]
-    alt = lla_array[2]
+    lat, long, alt = lla_array
 
     # convert lat/long from degrees to radians
     lat = deg2rad(lat)
@@ -42,3 +40,19 @@ def lla_to_ecef(lla_array):
         r_s*cos(lambda_s)*sin(long) + alt*cos(lat)*sin(long),
         r_s*sin(lambda_s) + alt*sin(lat)
     ])
+
+
+def ecef_to_lla(ecef_array):
+    '''
+    Convert from (x, y, z) [m] position in Earth-Centered, Earth-Fixed
+    frame to (latitude, longitude, altitude) [deg, deg, m] array in LLA.
+
+    Reference:
+    https://www.mathworks.com/help/aeroblks/ecefpositiontolla.html
+
+    :param ecef_array: `numpy.array([1x3])` array of (x, y, z) [m] as `float`
+    :return: `numpy.array([1x3])` LLA (lat, long, alt) [deg, deg, m] position
+    '''
+    # unpack ecef array
+    x, y, z = ecef_array
+    long = arctan(ecef[1)
